df2d71a3130ad5e7d16fa67dda4bdf8a7950da3b
Kareem <kareem.olusola@gmail.com>
2025-04-18 22:51:32 -0500

    testing

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "tfstate-rg"
+    resource_group_name  = "GeneralRG"
     storage_account_name = "tfstateaccount041825"
     container_name      = "tfstate"
     key                 = "resume.tfstate"
