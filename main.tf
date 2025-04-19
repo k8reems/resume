b120b28002d5e110b5bdc0cc53668085d1823533
Kareem <kareem.olusola@gmail.com>
2025-04-18 20:48:51 -0500

    publishing infra and profile app

---
+++ main.tf
@@ -0,0 +1,25 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "resume" {
+  name     = "resume-rg"
+  location = "centralus"
+}
+
+resource "azurerm_storage_account" "resume" {
+  name                     = "resumeweb${lower(substr(md5(azurerm_resource_group.resume.name), 0, 8))}"
+  resource_group_name      = azurerm_resource_group.resume.name
+  location                 = azurerm_resource_group.resume.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  account_kind             = "StorageV2"
+
+  static_website {
+    index_document = "index.html"
+  }
+}
+
+output "website_url" {
+  value = azurerm_storage_account.resume.primary_web_endpoint
+}
